3a28acf0c158151109bed79065f957b28b4a2c17
kilavila <christer.kilavik@protonmail.com>
2024-09-25 23:57:32 +0200

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>CV | Christer Kilavik</title>
+    </head>
+    <body>
+
+        <h1>Christer Kilavik</h1>
+
+    </body>
+</html>
